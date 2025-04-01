2aab39b65f8e3ecf8614ce89fe3dc6e9d1e9f282
Lucas Santana <lucasfdesantana095@gmail.com>
2025-04-01 08:48:11 -0300

    update desafio.py - 2025-04-01

--- desafio.py
+++ desafio.py
@@ -1,11 +1,17 @@
 import random 
 numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#print("Adivinhe o número de 1 a 10")
-#print("-"*32)
+print("Adivinhe o número de 1 a 10")
+print("-"*32)
 maquina = (random.choice(numero))
 jogador = input('Informe um número de um a dez: ')
 print(jogador)
-if jogador == maquina:
-    print('Você acertou!')
-else:
+while jogador != maquina:
     print('Você errou :/')
+    print("Tente novamente: ")
+    jogador = input('Informe um número de um a dez: ')
+    if jogador == maquina:
+        print('Você acertou!')
+        break
+    else:
+        continue
+    break
